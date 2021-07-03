86838853c6089327ff45d9c8148f62e47c0d9092
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-07-04 01:25:05 +1000

    feat: export UrlExtensions

--- packages/extensions-core/src/index.ts
+++ packages/extensions-core/src/index.ts
@@ -3,6 +3,7 @@ export * from "./extensions/TimestampExtensions";
 export * from "./models/Guid";
 export * from "./extensions/GuidExtensions";
 export * from "./models/Url";
+export * from "./extensions/UrlExtensions";
 export * from "./models/BaseError";
 export * from "./models/NotFoundError";
 export * from "./models/Enum";
