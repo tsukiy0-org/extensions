1c57332c7abb4afc2db7498ed93eda8af193a98a
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-07-04 12:36:05 +1000

    feat: LoggerMiddleware is a class that includes setting up a logger name

--- packages/extensions-express/src/middlewares/LoggerMiddleware.ts
+++ packages/extensions-express/src/middlewares/LoggerMiddleware.ts
@@ -1,40 +1,46 @@
 import { RequestHandler, Response } from "express";
 import { RequestCorrelationService } from "../services/RequestCorrelationService";
 import { WinstonLogger } from "@tsukiy0/extensions-logging-winston";
-import { ILogger } from "@tsukiy0/extensions-core";
+import { GuidExtensions, ILogger } from "@tsukiy0/extensions-core";
 
 export class LoggerMiddleware {
-  private static readonly key = "logger";
+  private readonly key: string;
+  constructor(private readonly name: string) {
+    this.key = this.buildKey(name);
+  }
 
-  static handler =
-    (name: string): RequestHandler =>
-    (req, res, next) => {
-      const correlationService = new RequestCorrelationService(req);
+  handler: RequestHandler = (req, res, next) => {
+    const correlationService = new RequestCorrelationService(req);
 
-      const logger = new WinstonLogger(name, correlationService, []);
+    const logger = new WinstonLogger(this.name, correlationService, []);
 
-      res.locals[LoggerMiddleware.key] = logger;
+    res.locals[this.key] = logger;
 
-      res.on("finish", () => {
-        logger.info("TRANSACTION", {
-          request: {
-            method: req.method,
-            path: req.path,
-            url: req.url,
-            query: req.query,
-            headers: req.headers,
-            body: req.body,
-          },
-          response: {
-            status: res.statusCode,
-          },
-        });
+    res.on("finish", () => {
+      logger.info("TRANSACTION", {
+        request: {
+          method: req.method,
+          path: req.path,
+          url: req.url,
+          query: req.query,
+          headers: req.headers,
+          body: req.body,
+        },
+        response: {
+          status: res.statusCode,
+        },
       });
+    });
 
-      next();
-    };
+    next();
+  };
 
-  static getLogger = (res: Response): ILogger => {
-    return res.locals[LoggerMiddleware.key] as ILogger;
+  getLogger = (res: Response): ILogger => {
+    return res.locals[this.key] as ILogger;
+  };
+
+  private readonly buildKey = (name: string): string => {
+    const id = GuidExtensions.generate();
+    return `logger_${name}_${id}`;
   };
 }
