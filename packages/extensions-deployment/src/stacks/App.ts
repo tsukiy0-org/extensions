55e14ac275b084e853d5c3a9bb6bc0d193d1d4b3
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-07-05 20:25:30 +1000

    test: export ApiUrl

--- packages/extensions-deployment/src/stacks/App.ts
+++ packages/extensions-deployment/src/stacks/App.ts
@@ -1,10 +1,14 @@
-import { Construct, Stack, StackProps } from "@aws-cdk/core";
+import { CfnOutput, Construct, Stack, StackProps } from "@aws-cdk/core";
 import { Api } from "../constructs/Api";
 
 export class App extends Stack {
   public constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    new Api(this, "Api");
+    const api = new Api(this, "Api");
+
+    new CfnOutput(this, "ApiUrl", {
+      value: api.url,
+    });
   }
 }
