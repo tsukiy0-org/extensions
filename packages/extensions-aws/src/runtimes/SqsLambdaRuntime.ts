3f000e80d1631721a5f5ddc1facf98ca3df0ae34
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-07-25 00:49:15 +1000

    feat: add SqsLambdaRuntime

---
+++ packages/extensions-aws/src/runtimes/SqsLambdaRuntime.ts
@@ -0,0 +1,37 @@
+import {
+  ICorrelationService,
+  NotFoundError,
+  StaticCorrelationService,
+} from "@tsukiy0/extensions-core";
+import { SQSHandler } from "aws-lambda";
+import { SqsLambdaCorrelationService } from "../services/SqsLambdaCorrelationService";
+import { WinstonLogger } from "@tsukiy0/extensions-logging-winston";
+
+export abstract class SqsLambdaRuntime<T> {
+  protected correlationService: ICorrelationService =
+    new StaticCorrelationService();
+
+  protected abstract handle: (message: T) => Promise<void>;
+
+  handler: SQSHandler = async (event) => {
+    const record = event.Records[0];
+
+    if (!record) {
+      throw new SqsRecordNotFoundError();
+    }
+
+    const correlationService = new SqsLambdaCorrelationService(record);
+    const logger = new WinstonLogger("SqsLambdaRuntime", correlationService);
+
+    try {
+      const message = JSON.parse(record.body);
+
+      await this.handle(message);
+    } catch (e) {
+      logger.error(e);
+      throw e;
+    }
+  };
+}
+
+export class SqsRecordNotFoundError extends NotFoundError {}
