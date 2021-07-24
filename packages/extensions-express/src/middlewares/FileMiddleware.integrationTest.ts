082eeb03502070dd137d6d0895056d836bdbb835
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-07-24 22:10:31 +1000

    test: fix test names

--- packages/extensions-express/src/middlewares/FileMiddleware.integrationTest.ts
+++ packages/extensions-express/src/middlewares/FileMiddleware.integrationTest.ts
@@ -9,19 +9,19 @@ describe("FileMiddleware", () => {
   const configuration = new SystemConfiguration();
   const apiUrl = Url.check(configuration.get("API_URL"));
 
-  it("caches *.jpg glob correctly", async () => {
+  it("caches *.png glob correctly", async () => {
     const url = UrlExtensions.appendPath(apiUrl, "/static/image.png");
 
     const actual = await fetch(url);
 
-    expect(actual.headers.get("Cache-Control")).toEqual("max-age=100");
+    expect(actual.headers.get("Cache-Control")).toEqual("max-age=200");
   });
 
-  it("caches *.png glob correctly", async () => {
+  it("caches *.jpg glob correctly", async () => {
     const url = UrlExtensions.appendPath(apiUrl, "/static/image.jpg");
 
     const actual = await fetch(url);
 
-    expect(actual.headers.get("Cache-Control")).toEqual("max-age=200");
+    expect(actual.headers.get("Cache-Control")).toEqual("max-age=100");
   });
 });
