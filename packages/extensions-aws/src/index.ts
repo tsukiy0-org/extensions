e60bc0014f8be0408edfef5c6c2ba3b4c3933261
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-08-26 21:34:34 +1000

    feat: add BatchRuntime

--- packages/extensions-aws/src/index.ts
+++ packages/extensions-aws/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./services/SqsMessageQueue";
-export * from "./runtimes/SqsLambdaRuntime";
-export * from "./runtimes/ExpressLambdaRuntime";
 export * from "./services/DefaultProcessor";
 export * from "./services/BatchMessageQueue";
+
+export * from "./runtimes/ExpressLambdaRuntime";
+export * from "./runtimes/SqsLambdaRuntime";
+export * from "./runtimes/BatchRuntime";
---
+++ packages/extensions-aws/src/runtimes/BatchRuntime.ts
@@ -0,0 +1,14 @@
+import { IProcessor, SystemConfiguration } from "@tsukiy0/extensions-core";
+import { BatchMessageQueue } from "../services/BatchMessageQueue";
+
+export abstract class BatchRuntime<T> {
+  constructor(private readonly processor: IProcessor<T, void>) {}
+
+  run = async (): Promise<void> => {
+    const config = new SystemConfiguration();
+    const rawMessage = config.get(BatchMessageQueue.MESSAGE_KEY);
+    const message = JSON.parse(rawMessage);
+
+    await this.processor.run(message);
+  };
+}
