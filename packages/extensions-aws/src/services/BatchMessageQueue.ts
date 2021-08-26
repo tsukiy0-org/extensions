711867761390ac6b01e2774255c1c80a713c7745
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-08-26 21:26:24 +1000

    feat: BatchMessageQueue starts job with uniq env variable with message

--- packages/extensions-aws/src/services/BatchMessageQueue.ts
+++ packages/extensions-aws/src/services/BatchMessageQueue.ts
@@ -12,6 +12,9 @@ type Config = {
 };
 
 export class BatchMessageQueue<T> implements IMessageQueue<T> {
+  public static readonly MESSAGE_KEY =
+    "MESSAGE__937ce2c8dd7001baec1785cb8dce6e6fe1bcf61f";
+
   constructor(
     private readonly client: Batch,
     private readonly config: Config,
@@ -27,8 +30,13 @@ export class BatchMessageQueue<T> implements IMessageQueue<T> {
         jobName: GuidExtensions.generate(),
         jobQueue: this.config.jobQueue,
         jobDefinition: this.config.jobDefinition,
-        parameters: {
-          message: JSON.stringify(message),
+        containerOverrides: {
+          environment: [
+            {
+              name: BatchMessageQueue.MESSAGE_KEY,
+              value: JSON.stringify(message),
+            },
+          ],
         },
       })
       .promise();
