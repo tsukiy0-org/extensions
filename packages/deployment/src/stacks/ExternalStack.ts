ab26b7514593cd0f39c9fc02d340ce75c1a77b66
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-09-06 20:44:12 +1000

    chore: delete PublicDbInstance

--- packages/deployment/src/stacks/ExternalStack.ts
+++ packages/deployment/src/stacks/ExternalStack.ts
@@ -1,33 +1,26 @@
-import { PublicDbInstance } from "@tsukiy0/extensions-aws-cdk";
-import { RemovalPolicy, SecretValue, Stack, StackProps } from "aws-cdk-lib";
-import {
-  Credentials,
-  DatabaseInstanceEngine,
-  PostgresEngineVersion,
-  StorageType,
-} from "aws-cdk-lib/lib/aws-rds";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 
 export class ExternalStack extends Stack {
   public constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    new PublicDbInstance(this, "PublicDbInstance", {
-      databaseInstance: {
-        engine: DatabaseInstanceEngine.postgres({
-          version: PostgresEngineVersion.VER_13_3,
-        }),
-        credentials: Credentials.fromPassword(
-          "postgres",
-          SecretValue.plainText("postgres"),
-        ),
-        port: 5432,
+    // new PublicDbInstance(this, "PublicDbInstance", {
+    //   databaseInstance: {
+    //     engine: DatabaseInstanceEngine.postgres({
+    //       version: PostgresEngineVersion.VER_13_3,
+    //     }),
+    //     credentials: Credentials.fromPassword(
+    //       "postgres",
+    //       SecretValue.plainText("postgres"),
+    //     ),
+    //     port: 5432,
 
-        // delete cleanly
-        removalPolicy: RemovalPolicy.DESTROY,
-        deletionProtection: false,
-        deleteAutomatedBackups: true,
-      },
-    });
+    //     // delete cleanly
+    //     removalPolicy: RemovalPolicy.DESTROY,
+    //     deletionProtection: false,
+    //     deleteAutomatedBackups: true,
+    //   },
+    // });
   }
 }
