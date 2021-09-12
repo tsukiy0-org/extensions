c61141a16d799606098a0b36a200356d75712307
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-09-12 12:22:18 +1000

    fix: remove removal policy from fn

--- packages/extensions-aws-cdk/src/constructs/DefaultFunction.ts
+++ packages/extensions-aws-cdk/src/constructs/DefaultFunction.ts
@@ -1,9 +1,9 @@
-import { Duration, RemovalPolicy } from "aws-cdk-lib";
+import { Duration } from "aws-cdk-lib";
 import {
   Function as LambdaFunction,
   FunctionProps,
 } from "aws-cdk-lib/lib/aws-lambda";
-import { LogGroup, RetentionDays } from "aws-cdk-lib/lib/aws-logs";
+import { RetentionDays } from "aws-cdk-lib/lib/aws-logs";
 import { Construct } from "constructs";
 
 export class DefaultFunction extends LambdaFunction {
@@ -20,7 +20,5 @@ export class DefaultFunction extends LambdaFunction {
       retryAttempts: 0,
       ...props,
     });
-
-    (this.logGroup as LogGroup).applyRemovalPolicy(RemovalPolicy.DESTROY);
   }
 }
