e4ec0d3d678846aef9311d17c3909341927a9e03
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-09-15 17:03:35 +1000

    feat: remove middleware in favor of processor pattern

--- packages/extensions-express/src/index.ts
+++ packages/extensions-express/src/index.ts
@@ -1,9 +1,5 @@
 export * from "./helpers/promisifyHandler";
 
-export * from "./middlewares/LoggerMiddleware";
-export * from "./middlewares/ErrorMiddleware";
-export * from "./middlewares/AbstractServicesMiddleware";
-export * from "./middlewares/CorrelationMiddleware";
 export * from "./middlewares/FileMiddleware";
 export * from "./middlewares/ApiKeyAuthMiddleware";
 
---
+++ packages/extensions-express/src/runtimes/ExpressJsonRuntime.test.ts
@@ -0,0 +1,90 @@
+import {
+  GuidExtensions,
+  SystemConfiguration,
+  Url,
+  UrlExtensions,
+} from "@tsukiy0/extensions-core";
+
+describe("ExpressJsonRuntime", () => {
+  const configuration = new SystemConfiguration();
+  const apiUrl = Url.check(configuration.get("API_URL"));
+
+  describe("processor services", () => {
+    it("resolves services", async () => {
+      const url = UrlExtensions.appendPath(apiUrl, "/services");
+
+      const actual = await fetch(url);
+
+      expect(await actual.json()).toEqual({
+        testService: "test",
+        correlationService: expect.any(Object),
+      });
+    });
+  });
+
+  describe("handles errors", () => {
+    it("when UnauthorizedError then status is 401", async () => {
+      const url = UrlExtensions.appendPath(apiUrl, "/errors/unauthorized");
+
+      const actual = await fetch(url);
+
+      expect(actual.status).toEqual(401);
+    });
+
+    it("when ValidationError then status is 400", async () => {
+      const url = UrlExtensions.appendPath(apiUrl, "/errors/validation");
+
+      const actual = await fetch(url);
+
+      expect(actual.status).toEqual(400);
+    });
+
+    it("when RuntypesValidationError then status is 400", async () => {
+      const url = UrlExtensions.appendPath(
+        apiUrl,
+        "/errors/validation/runtypes",
+      );
+
+      const actual = await fetch(url);
+
+      expect(actual.status).toEqual(400);
+    });
+
+    it("when unknown then status is 500", async () => {
+      const url = UrlExtensions.appendPath(apiUrl, "/errors/unknown");
+
+      const actual = await fetch(url);
+
+      expect(actual.status).toEqual(500);
+    });
+  });
+
+  describe("correlation", () => {
+    it("when send trace id then returns that", async () => {
+      const url = UrlExtensions.appendPath(apiUrl, "/correlation");
+      const traceId = GuidExtensions.generate();
+
+      const actual = await fetch(url, {
+        headers: {
+          "x-trace-id": traceId,
+        },
+      });
+
+      expect(await actual.json()).toEqual({
+        traceId,
+        spanId: expect.any(String),
+      });
+    });
+
+    it("when no trace id then returns random", async () => {
+      const url = UrlExtensions.appendPath(apiUrl, "/correlation");
+
+      const actual = await fetch(url);
+
+      expect(await actual.json()).toEqual({
+        traceId: expect.any(String),
+        spanId: expect.any(String),
+      });
+    });
+  });
+});
