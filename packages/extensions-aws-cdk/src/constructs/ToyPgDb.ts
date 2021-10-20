ce1f7658c837c8110997450802a7a9c350a4eab2
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-10-21 08:40:24 +1100

    chore: remove user data replaces instance

--- packages/extensions-aws-cdk/src/constructs/ToyPgDb.ts
+++ packages/extensions-aws-cdk/src/constructs/ToyPgDb.ts
@@ -60,36 +60,35 @@ export class ToyPgDb extends Construct {
         generation: AmazonLinuxGeneration.AMAZON_LINUX_2,
       }),
       keyName: props.keyName,
-      userDataCausesReplacement: true,
-      init: CloudFormationInit.fromElements(
-        InitFile.fromString(
-          "/etc/yum.repos.d/pgdg.repo",
-          `[pgdg13]
-      name=PostgreSQL 13 for RHEL/CentOS 7 - x86_64
-      baseurl=https://download.postgresql.org/pub/repos/yum/13/redhat/rhel-7-x86_64
-      enabled=1
-      gpgcheck=0`,
-        ),
-        InitCommand.shellCommand("sudo yum update -y"),
-        InitCommand.shellCommand(
-          "sudo yum -y install postgresql13 postgresql13-server",
-        ),
-        InitCommand.shellCommand(
-          "sudo /usr/pgsql-13/bin/postgresql-13-setup initdb",
-        ),
-        InitCommand.shellCommand(`sudo systemctl start postgresql-13`),
-        InitCommand.shellCommand(`sudo systemctl enable postgresql-13`),
-        InitCommand.shellCommand(
-          `sudo -u postgres psql -c "ALTER USER postgres PASSWORD '${props.password}';`,
-        ),
-        InitCommand.shellCommand(
-          `sudo echo "listen_addresses = '*'" >> /var/lib/pgsql/13/data/postgresql.conf`,
-        ),
-        InitCommand.shellCommand(
-          `sudo echo "host all all 0.0.0.0/0 md5" >> /var/lib/pgsql/13/data/pg_hba.conf`,
-        ),
-        InitCommand.shellCommand(`sudo systemctl restart postgresql-13`),
-      ),
+      // init: CloudFormationInit.fromElements(
+      //   InitFile.fromString(
+      //     "/etc/yum.repos.d/pgdg.repo",
+      //     `[pgdg13]
+      // name=PostgreSQL 13 for RHEL/CentOS 7 - x86_64
+      // baseurl=https://download.postgresql.org/pub/repos/yum/13/redhat/rhel-7-x86_64
+      // enabled=1
+      // gpgcheck=0`,
+      //   ),
+      //   InitCommand.shellCommand("sudo yum update -y"),
+      //   InitCommand.shellCommand(
+      //     "sudo yum -y install postgresql13 postgresql13-server",
+      //   ),
+      //   InitCommand.shellCommand(
+      //     "sudo /usr/pgsql-13/bin/postgresql-13-setup initdb",
+      //   ),
+      //   InitCommand.shellCommand(`sudo systemctl start postgresql-13`),
+      //   InitCommand.shellCommand(`sudo systemctl enable postgresql-13`),
+      //   InitCommand.shellCommand(
+      //     `sudo -u postgres psql -c "ALTER USER postgres PASSWORD '${props.password}';`,
+      //   ),
+      //   InitCommand.shellCommand(
+      //     `sudo echo "listen_addresses = '*'" >> /var/lib/pgsql/13/data/postgresql.conf`,
+      //   ),
+      //   InitCommand.shellCommand(
+      //     `sudo echo "host all all 0.0.0.0/0 md5" >> /var/lib/pgsql/13/data/pg_hba.conf`,
+      //   ),
+      //   InitCommand.shellCommand(`sudo systemctl restart postgresql-13`),
+      // ),
     });
   }
 }
